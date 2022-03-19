5288160ba5a222a8ad7809fbcd8002823fadab29
gianniskorkontzilas <gianniskorkontzilas@hotmail.com>
2022-03-19 20:00:35 +0200

    Create PropertyService Interface

--- src/main/java/com/technico/technicoproject/service/PropertyService.java
+++ src/main/java/com/technico/technicoproject/service/PropertyService.java
@@ -1,4 +1,15 @@
 package com.technico.technicoproject.service;
 
+
+import com.technico.technicoproject.model.Property;
+
+import java.util.List;
+
 public interface PropertyService {
+    Property createProperty(Property property);
+    Property readPropertyByPropertyNumber(String propertyNumber);
+    List<Property> readPropertyByVatNumber(String vatNumber);
+    Property updateProperty(String propertyNumber , Property property);
+    boolean deleteProperty(String propertyNumber);
+
 }
