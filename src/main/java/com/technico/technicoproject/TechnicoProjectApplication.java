edb0a1d5b8d36731719774f0ba16fb551e000e2c
Ioannis Seitanidis <ioan.seitanidis@gmail.com>
2022-03-19 17:32:57 +0200

    Initial commit

---
+++ src/main/java/com/technico/technicoproject/TechnicoProjectApplication.java
@@ -0,0 +1,13 @@
+package com.technico.technicoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicoProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnicoProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/technico/technicoproject/TechnicoProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technico.technicoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicoProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
