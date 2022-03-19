ebd985a5a658356a4a2b9785044ae90d17ec6e64
evigiannakopoulou <evi.gk2@hotmail.com>
2022-03-19 18:44:17 +0200

    Created OwnerRepository.java

--- src/main/java/com/technico/technicoproject/repository/OwnerRepository.java
+++ src/main/java/com/technico/technicoproject/repository/OwnerRepository.java
@@ -1,6 +1,13 @@
 package com.technico.technicoproject.repository;
 
+import com.technico.technicoproject.model.Owner;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public class OwnerRepository  {
+@Repository
+public interface OwnerRepository extends JpaRepository<Owner, String> {
+    Owner findOwnerByVatNumber(String vatNumber);
+    Owner findOwnerByEmail(String email);
+    boolean existsOwnersByVatNumber(String vatNumber);
 }
+
