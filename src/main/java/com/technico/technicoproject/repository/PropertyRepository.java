66ab53761e67fa029fd0551147f56d033098359c
LamprosPetridis <lamprosp98@gmail.com>
2022-03-19 19:48:20 +0200

    Implemented PropertyRepository.java interface.

--- src/main/java/com/technico/technicoproject/repository/PropertyRepository.java
+++ src/main/java/com/technico/technicoproject/repository/PropertyRepository.java
@@ -1,4 +1,9 @@
 package com.technico.technicoproject.repository;
 
-public class PropertyRepository {
+import com.technico.technicoproject.model.Property;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PropertyRepository extends JpaRepository<Property, String> {
+    Property findPropertyByEmail(String email);
+    boolean existsPropertyByVatNumber(String vatNumber);
 }
